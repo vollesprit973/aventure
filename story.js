6f13aab95cb9701f8ebbb3bca06b4e19aef176aa
vollesprit973 <crepusculle77@gmail.com>
2026-02-12 16:12:57 -0300

    Update story.js

--- story.js
+++ story.js
@@ -5,7 +5,7 @@ image:"images/Entree.webp",
 text:"Apres une tres longue journée de travail, tu rentres chez toi. Tu ne sais pas si ta femme est chez toi ou chez un ami.",
 choices:[
 {label:"Entrer", next:"Entrer01"},
-{label:"Envoyer un message", next:"SMS01"}
+{label:"Envoyer un message", next:"SMS01"},
 {label:"Appeller", next:"tel01"}
 ]
 },
@@ -15,7 +15,7 @@ image:"images/Entree.webp",
 text:"Tu entends du bruit dans la maison.",
 choices:[
 {label:"Ca vient du salon", next:"Salon01"},
-{label:"Ca vient de la chambre", next:"chb01"}
+{label:"Ca vient de la chambre", next:"chb01"},
 {label:"Ca vient de la salle de bain", next:"SdB01"}
 ]
 },
@@ -25,7 +25,7 @@ image:"images/SMS01.jfif",
 text:"Tu patientes quelques secondes et :",
 choices:[
 {label:"Elle répond, tu vas pouvoir lui demander ce qu'elle fait", next:"SMS02"},
-{label:"Elle ne repond pas, tu l'appelles", next:"tel01"}
+{label:"Elle ne repond pas, tu l'appelles", next:"tel01"},
 {label:"Elle ne repond pas, tu rentre", next:"Entrer01"}
 ]
 },
